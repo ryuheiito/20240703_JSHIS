dc125b3137ea8c3c7a57b86c8eb25defad6e7b76
ryuheiito <81726237+ryuheiito@users.noreply.github.com>
2024-07-02 22:15:44 +0900

    Add files via upload

---
+++ bat.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# 1の実行
+subprocess.run(["python", "1_JSHIS_RPA.py"])
+
+# 2の実行
+subprocess.run(["python", "2_JSHIS_RPA.py"])
+
+# 3の実行
+subprocess.run(["python", "3_JSHIS_RPA.py"])
+
